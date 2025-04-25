72ae285e5fbe1254d8b8fde96604148a47115406
elio k :) <156268419+kelloeon@users.noreply.github.com>
2025-04-24 22:04:10 -0700

    LAB 5 WORK
    filled in my answers to the questions and created a javascript program to describe the make and model of my family's car (as per task 1 and 2)

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 Task 2
+// Author: Elio Kim
+// Date: April 24, 2025
+// License: Public Domain
 
-// Constants
 
-// Functions
+//Define Variables:
+make = "Honda";
+model = "Odyssey"
+color = "silver";
+year = 2008;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Calculations:
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+age = 2025 - year;
 
-// let's get this party started
-main();
+//Outputs:
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years" + "<br>");
+
+
